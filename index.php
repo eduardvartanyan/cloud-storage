811c8b54538ef4880f88b90bd49ceaf23ff2c55e
Eduard Vartanyan <evartanyan1989@gmail.com>
2022-08-10 13:20:20 +0800

    Task 5 done

--- index.php
+++ index.php
@@ -45,11 +45,12 @@ $urlList = [
     ],
 ];
 
+$isValidRequest = false;
+
 if (isset($_GET) && !empty($_GET)) {
 
     $keysGetArray = array_keys($_GET);
     $requestMethod = $_SERVER['REQUEST_METHOD'];
-
     $func = 'Метод не найден';
 
     foreach ($urlList as $key => $url) {
@@ -57,27 +58,40 @@ if (isset($_GET) && !empty($_GET)) {
         $patternWithId = '/^(' . str_replace('/', '\/', $key) . ')[0-9]\/$/';
         $patternWithoutId = '/^(' . str_replace('/', '\/', $key) . ')$/';
 
-        if (isset($url[$requestMethod]) && !empty($url[$requestMethod])) {
+        if (isset($url[$requestMethod])) {
 
             if (preg_match($patternWithId, $keysGetArray[0])) {
+
+                $isValidRequest = true;
 
                 $func = $url[$requestMethod];
                 $urlArray = explode('/', $keysGetArray[0]);
                 $id = $urlArray[count($urlArray) - 2];
+                parse_str(file_get_contents('php://input'), $_PUT);
 
-                var_dump($func($id));
+                var_dump($func(array_merge($_GET, $_PUT), $id));
 
             } elseif (preg_match($patternWithoutId, $keysGetArray[0])) {
 
-                $func = $url[$requestMethod];
+                $isValidRequest = true;
 
-                var_dump($func());
+                $func = $url[$requestMethod];
+                parse_str(file_get_contents('php://input'), $_PUT);
+
+                var_dump($func(array_merge($_GET, $_PUT)));
 
             }
 
         }
 
     }
+
+}
+
+if (!$isValidRequest) {
+
+    http_response_code(405);
+    var_dump(false);
 
 }
 
